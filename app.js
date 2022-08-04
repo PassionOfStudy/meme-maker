25a5d24b92acdd441dea260192aa9e2552cb029d
PassionOfStudy <35111721+PassionOfStudy@users.noreply.github.com>
2022-08-04 17:47:49 +0900

    Drawing Project One : Home

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
+ctx.fillRect(200, 200, 50, 150);
+ctx.fillRect(400, 200, 50, 150);
+ctx.lineWidth = 5;
+ctx.strokeRect(300, 275, 50, 75);
+ctx.fillRect(200, 200, 250, 10);
+ctx.beginPath();
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 150);
+ctx.lineTo(450, 200);
 ctx.stroke();
